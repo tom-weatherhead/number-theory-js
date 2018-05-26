a9c5f16408abb38a1bec1ad5a320ea02511e8b5f
Tom Weatherhead <tom_weatherhead@hotmail.com>
2018-05-26 13:22:30 -0600

    More dev

--- test/number-theory-js-test.js
+++ test/number-theory-js-test.js
@@ -1,9 +1,17 @@
-const path = require('path');
-const engine = require(path.join(__dirname, '..', 'lib', 'number-theory-js.js'));	// Or just: require('..'); ?
+//const path = require('path');
+//const engine = require(path.join(__dirname, '..', 'lib', 'number-theory-js.js'));	// Or just: require('..'); ?
+const engine = require('..');
 
 exports.lifecycle = {
 	test: function (test) {
-		test.expect(18);
+		test.expect(23);
+
+		// console.log(engine.condenseFactorList(engine.factorize(72)));	// { '2': 3, '3': 2 }
+		// console.log(engine.condenseFactorList(engine.factorize(2520))); // { '2': 3, '3': 2, '5': 1, '7': 1 }
+
+		// console.log('gcdExtended final result:', engine.gcdExtended(254, 38));
+
+		// console.log(engine.divisors(28));
 
 		test.equal(engine.gcd(35, 84),				7,										'Should be 7');
 		test.equal(engine.gcd(24, 80),				8,										'Should be 8');
@@ -23,6 +31,13 @@ exports.lifecycle = {
 		test.deepEqual(engine.factorize(6),			[2, 3],									'Should be [2, 3]');
 		test.deepEqual(engine.factorize(8),			[2, 2, 2],								'Should be [2, 2, 2]');
 		test.deepEqual(engine.factorize(72),		[2, 2, 2, 3, 3],						'Should be [2, 2, 2, 3, 3]');
+
+		test.deepEqual(engine.condenseFactorList(engine.factorize(72)),	{ '2': 3, '3': 2 },					'Should be { \'2\': 3, \'3\': 2 }');
+		test.deepEqual(engine.condenseFactorList(engine.factorize(2520)),	{ '2': 3, '3': 2, '5': 1, '7': 1 },	'Should be { \'2\': 3, \'3\': 2, \'5\': 1, \'7\': 1 }');
+
+		test.ok(engine.isPerfect(28), 'Should be true');
+		test.ok(!engine.isPerfect(100), 'Should be true');
+		test.ok(engine.isPerfect(496), 'Should be true');
 
 		test.equal(engine.totient(11),				10,										'Should be 10');
 		test.equal(engine.totient(12),				4,										'Should be 4');
